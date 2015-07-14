b21d556ef4b214b2c19badee9d57eebe70791dad
AlanYang1 <qtjyag@163.com>
2015-07-14 17:26:46 +0800

    Version Date: 2015-07-14 5:30PM

--- SQLNavigator/AutoTest/AutoFiles/CodeEditor/Syntax/CodeCompletion/CreatePACKAGE.sql
+++ SQLNavigator/AutoTest/AutoFiles/CodeEditor/Syntax/CodeCompletion/CreatePACKAGE.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE 
 PACKAGE auto_pkg_codeCompletion
   IS
-Type all_tab_type is table of emp%rowtype; 
+Type all_tab_type is table of auto_tab_emp%rowtype; 
    FUNCTION function_name(a NUMBER,b NUMBER)
      RETURN  all_tab_type pipelined;
 END; -- Package spec
@@ -14,7 +14,7 @@ IS
      RETURN  all_tab_type pipelined
     is
     begin 
-      for cur in (select * from emp) loop 
+      for cur in (select * from auto_tab_emp) loop 
         pipe row(cur); 
       end loop; 
       return; 
